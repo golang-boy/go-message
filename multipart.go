1e11d7bf0d613168a6289e20c6f76b9985a5d013
golang-boy <58363816+golang-boy@users.noreply.github.com>
2021-08-03 17:41:58 +0800

    Update multipart.go

--- multipart.go
+++ multipart.go
@@ -3,7 +3,7 @@ package message
 import (
 	"io"
 
-	"github.com/emersion/go-message/textproto"
+	"github.com/golang-boy/go-message/textproto"
 )
 
 // MultipartReader is an iterator over parts in a MIME multipart body.
